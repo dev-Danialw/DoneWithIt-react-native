aea7aba55ba77c2c3348185e2fbe98fc1403970b
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-11 00:13:10 +0500

    added icons in view imageScreen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
