9f1e0d723cc8fde94c92566787fc5166f5b0e51a
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-11 00:03:36 +0500

    built ListItem

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
@@ -11,6 +12,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red Jackter for Sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mosh Mesh"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -33,6 +41,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "500",
     marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
