be249e2faccbb4e8f6b3cf9566553e7618beffdd
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-12 18:11:34 +0500

    created account screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -3,8 +3,8 @@ import Constants from "expo-constants";
 import { StyleSheet } from "react-native";
 import { SafeAreaView, ShadowPropTypesIOS } from "react-native";
 
-function Screen({ children }) {
-  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+function Screen({ children, style }) {
+  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
