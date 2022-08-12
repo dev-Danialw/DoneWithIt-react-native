f016a48fd38f84fb8ea9ae33b0817acbd6f3f159
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-12 20:20:41 +0500

    built a textInput

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessageScreen from "./app/screens/MessageScreen";
+import ListingScreen from "./app/screens/ListingScreen";
 
 export default function App() {
-  return <MessageScreen />;
+  return <ListingScreen />;
 }
