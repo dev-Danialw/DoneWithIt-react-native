ad8ef4c2a48ddcd00286e387cb911e2645352441
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-12 18:38:05 +0500

    built the listingscreen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Constants from "expo-constants";
-import { StyleSheet } from "react-native";
-import { SafeAreaView, ShadowPropTypesIOS } from "react-native";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 
 function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
