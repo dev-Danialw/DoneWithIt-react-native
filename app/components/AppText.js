bd1ff3c130a13c95c56c4b5e19e2d98e5fde3ae4
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-13 18:37:18 +0500

    handled long text

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Text } from "react-native";
 import defautStyles from "../config/styles";
 
-function AppText({ children, style }) {
-  return <Text style={[defautStyles.text, style]}>{children}</Text>;
+function AppText({ children, style, ...otherProps }) {
+  return (
+    <Text style={[defautStyles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
 }
 
 export default AppText;
