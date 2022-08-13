0929654ab2ee407c31c33659e3497a3c7caef860
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-13 18:34:50 +0500

    added a chevron in listitem

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import Screen from "./app/components/Screen";
 import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 import LoginScreen from "./app/screens/LoginScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <ListingEditScreen />;
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
+import { Swipeable } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppText from "./AppText";
-import Swipeable from "react-native-gesture-handler/Swipeable";
-
 import colors from "../config/colors";
 
 function ListItem({
@@ -23,6 +23,11 @@ function ListItem({
             <AppText style={styles.title}>{title}</AppText>
             {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
+          <MaterialCommunityIcons
+            name="chevron-right"
+            size={25}
+            color={colors.medium}
+          />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -31,11 +36,13 @@ function ListItem({
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flexDirection: "row",
     padding: 15,
     backgroundColor: colors.white,
   },
   detailsContainer: {
+    flex: 1,
     marginLeft: 10,
     justifyContent: "center",
   },
